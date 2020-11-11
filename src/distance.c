a275da36c7ffdb449390d36d9a2489e8b50d7411
oserpe <oserpe@itba.edu.ar>
2020-11-11 18:15:05 -0300

    distance  y encriptador (no necesario) hechos, easter .RUN_ME

---
+++ src/distance.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+
+int main(){
+    char msg[100] ="ESTO ES UN EASTER EGG";
+    for(int i = 0 ; i < strlen("ESTO ES UN EASTER EGG") ; i++){
+        printf("%d ;",msg[i]-'A');
+    }
+    putchar('\n');
+}
